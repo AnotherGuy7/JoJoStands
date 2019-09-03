29e7e586ce6acff8a3eec6b22e027eaa43a57e53
AnotherGuy7 <50064874+AnotherGuy7@users.noreply.github.com>
2019-09-02 21:35:58 -0400

    v1.7

---
+++ Projectiles/Pets/TuskAct1Pet.cs
@@ -0,0 +1,115 @@
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Projectiles.Pets
+{
+    public class TuskAct1Pet : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projPet[projectile.type] = true;
+            Main.projFrames[projectile.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 24;
+            projectile.height = 26;
+            projectile.penetrate = -1;
+            projectile.netImportant = true;
+            projectile.friendly = true;
+            projectile.ignoreWater = true;
+            projectile.tileCollide = false;
+            projectile.manualDirectionChange = true;
+        }
+
+        public override void AI()       //I unfortunately had to copy the DD2Pet AI style cause framecounter was made to stay at 5 on it...
+        {
+            Player player = Main.player[projectile.owner];
+            MyPlayer modPlayer = player.GetModPlayer<MyPlayer>();
+            projectile.frameCounter++;
+            if (player.dead)
+            {
+                modPlayer.TuskAct1Pet = false;
+            }
+            if (modPlayer.TuskAct1Pet)
+            {
+                projectile.timeLeft = 2;
+            }
+            if (projectile.frameCounter >= 10)
+            {
+                projectile.frame += 1;
+                projectile.frameCounter = 0;
+            }
+            if (projectile.frame >= 4)
+            {
+                projectile.frame = 0;
+            }
+            float num = 4f;
+            Vector2 value = new Vector2((float)(player.direction * 30), -20f);
+            projectile.direction = (projectile.spriteDirection = player.direction);
+            Vector2 vector = player.MountedCenter + value;
+            float num6 = Vector2.Distance(projectile.Center, vector);
+            if (num6 > 1000f)
+            {
+                projectile.Center = player.Center + value;
+            }
+            Vector2 vector2 = vector - projectile.Center;
+            if (num6 < num)
+            {
+                projectile.velocity *= 0.25f;
+            }
+            if (vector2 != Vector2.Zero)
+            {
+                if (vector2.Length() < num * 0.5f)
+                {
+                    projectile.velocity = vector2;
+                }
+                else
+                {
+                    projectile.velocity = vector2 * 0.1f;
+                }
+            }
+            if (projectile.velocity.Length() > 6f)
+            {
+                float num7 = projectile.velocity.X * 0.08f + projectile.velocity.Y * (float)projectile.spriteDirection * 0.02f;
+                if (Math.Abs(projectile.rotation - num7) >= 3.14159274f)
+                {
+                    if (num7 < projectile.rotation)
+                    {
+                        projectile.rotation -= 6.28318548f;
+                    }
+                    else
+                    {
+                        projectile.rotation += 6.28318548f;
+                    }
+                }
+                float num8 = 12f;
+                projectile.rotation = (projectile.rotation * (num8 - 1f) + num7) / num8;
+            }
+            else
+            {
+                if (projectile.rotation > 3.14159274f)
+                {
+                    projectile.rotation -= 6.28318548f;
+                }
+                if (projectile.rotation > -0.005f && projectile.rotation < 0.005f)
+                {
+                    projectile.rotation = 0f;
+                }
+                else
+                {
+                    projectile.rotation *= 0.96f;
+                }
+            }
+            projectile.localAI[0] += 1f;
+            if (projectile.localAI[0] > 120f)
+            {
+                projectile.localAI[0] = 0f;
+            }
+        }
+    }
+}
---
+++ Projectiles/Pets/TuskAct3Pet.cs
@@ -0,0 +1,52 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Projectiles.Pets
+{
+    public class TuskAct3Pet : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.projPet[projectile.type] = true;
+            Main.projFrames[projectile.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 24;
+            projectile.height = 26;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[projectile.owner];
+            MyPlayer modPlayer = player.GetModPlayer<MyPlayer>();
+            projectile.frameCounter++;
+            if (player.dead)
+            {
+                modPlayer.TuskAct3Pet = false;
+            }
+            if (modPlayer.TuskAct3Pet)
+            {
+                projectile.timeLeft = 2;
+            }
+            Vector2 vector131 = player.Center;
+            vector131.X -= (float)((12 + player.width / 2) * player.direction);
+            vector131.Y -= 25f;
+            projectile.Center = Vector2.Lerp(projectile.Center, vector131, 0.2f);
+            projectile.velocity *= 0.8f;
+            projectile.direction = (projectile.spriteDirection = player.direction);
+            if (projectile.frameCounter >= 10)
+            {
+                projectile.frame += 1;
+                projectile.frameCounter = 0;
+            }
+            if (projectile.frame >= 4)
+            {
+                projectile.frame = 0;
+            }
+        }
+    }
+}
