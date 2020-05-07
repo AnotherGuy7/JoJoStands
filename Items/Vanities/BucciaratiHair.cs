e6cde4883d288178bc49c3d5dc9ddb95913f0f94
Ozi <magnusterrarian@gmail.com>
2020-05-08 02:30:02 +0300

    bucciarati

---
+++ Items/Vanities/BucciaratiHair.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Items.Vanities
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class BucciaratiHair : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Bucciarati's Hair");
+			Tooltip.SetDefault("The outside-in part of Doppio's Hair...");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 18;
+			item.height = 18;
+			item.rare = 6;
+			item.vanity = true;
+		}
+
+		public override bool DrawHead()
+		{
+			return true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Silk, 2);
+			recipe.AddTile(TileID.Loom);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
