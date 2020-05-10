65847612eb39df644f68629b7d37d4428c33e66e
Ozi <magnusterrarian@gmail.com>
2020-05-10 23:55:16 +0300

    M-MOHAMMEDU AVDOL?!
    YESSS! I AM!

---
+++ Items/Vanities/AvdolShirt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Items.Vanities
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class AvdolShirt : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Avdol's Shirt");
+			Tooltip.SetDefault("An open-front jacket. The undershirt is skintight.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 18;
+			item.height = 18;
+			item.rare = 6;
+			item.vanity = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Silk, 10);
+			recipe.AddTile(TileID.Loom);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
