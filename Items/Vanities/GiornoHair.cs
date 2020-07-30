35bf54ddd72f0e82afc9a5a7400c46c3fe46bc1a
Ozi <magnusterrarian@gmail.com>
2020-07-30 22:14:39 +0300

    Giorno vanities

---
+++ Items/Vanities/GiornoHair.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Items.Vanities
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class GiornoHair : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Italian Wig");
+			Tooltip.SetDefault("A wig, fashioned into a unique hairstyle with three loops in the front.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 18;
+			item.height = 18;
+			item.rare = 6;
+			item.vanity = true;
+		}
+
+		public override bool DrawHead()
+		{
+			return true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Silk, 2);
+			recipe.AddTile(TileID.Loom);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
