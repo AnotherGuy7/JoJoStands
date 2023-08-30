d7f35c334d9cb506a1756b9367ef48aaee4a5206
charz185 <80986803+charz185@users.noreply.github.com>
2023-08-30 12:20:44 -0400

    Mandom & pearl jam items

---
+++ Items/MandomT1.cs
@@ -0,0 +1,43 @@
+﻿using JoJoStands.Items.CraftingMaterials;
+using JoJoStands.Tiles;
+using JoJoStands.Networking;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Items
+{
+    public class MandomT1 : StandItemClass
+    {
+        public override int StandSpeed => 20;
+        public override int StandType => 0;
+        public override string StandProjectileName => "Mandom";
+        public override int StandTier => 1;
+
+        public override void SetStaticDefaults()
+        {
+            //DisplayName.SetDefault("Mandom (Tier 1)");
+            //Tooltip.SetDefault("Special: Rewind time by 6 seconds.");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 0;
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = 1;
+            Item.value = 0;
+            Item.noUseGraphic = true;
+            Item.rare = ItemRarityID.LightPurple;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<StandArrow>())
+                .AddIngredient(ModContent.ItemType<SoulofTime>(),3)
+                .AddIngredient(ModContent.ItemType<WillToFight>(),5)
+                .AddTile(ModContent.TileType<RemixTableTile>())
+                .Register();
+        }
+    }
+}
---
+++ Items/PearlJamT2.cs
@@ -0,0 +1,44 @@
+﻿using JoJoStands.Items.CraftingMaterials;
+using JoJoStands.Tiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Items
+{
+    public class PearlJamT2 : StandItemClass
+    {
+        public override int StandSpeed => 20;
+        public override int StandType => 2;
+        public override string StandProjectileName => "PearlJam";
+        public override int StandTier => 2;
+        public override string Texture
+        {
+            get { return Mod.Name + "/Items/PearlJamT1"; }
+        }
+        public override void SetStaticDefaults()
+        {
+            //DisplayName.SetDefault("Pearl Jam (Tier 2)");
+            //Tooltip.SetDefault("Heal yourself and others with even better food.");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 0;
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = 1;
+            Item.value = 0;
+            Item.noUseGraphic = true;
+            Item.rare = ItemRarityID.LightPurple;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.HealingPotion, 4)
+                .AddIngredient(ModContent.ItemType<WillToProtect>(),3)
+                .AddTile(ModContent.TileType<RemixTableTile>())
+                .Register();
+        }
+    }
+}
