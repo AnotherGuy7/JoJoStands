be6ed9bf82f3f7f8bc145cbdf5da223ae3716285
Jayl01 <50064874+AnotherGuy7@users.noreply.github.com>
2026-03-25 16:01:39 -0400

    Hey Ya Changes

--- Projectiles/PlayerStands/HeyYa/HeyYaStand.cs
+++ Projectiles/PlayerStands/HeyYa/HeyYaStand.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using ReLogic.Content;
+using System;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
