f5883faf47423b832e067280cd15109e5494e822
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2026-03-28 23:26:22 +0300

    Purple Haze base

---
+++ Projectiles/PlayerStands/PurpleHaze/PurpleHazeStand.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Projectiles.PlayerStands.PurpleHaze
+{
+    public abstract class PurpleHazeStand : StandClass
+    {
+        // —ability flags, no implementation we just got the gates, change later to more appropriate names
+        protected virtual bool CanReleaseVirus => false;
+        protected virtual bool CanInfectOnHit => false;
+        protected virtual bool CanAOEBurst => false;
+
+        // —tier overrides—
+        public override float MaxDistance => 98f;
+        public override int PunchDamage => 23;
+        public override int PunchTime => 11;
+        public override int HalfStandHeight => 37;
+        public override int FistID => 0;
+        public override int TierNumber => 1;
+        //public override string PunchSoundName => "ubasha";
+        public override string PoseSoundName => "PurpleHazePose";
+        public override string SpawnSoundName => "PurpleHaze";
+        public override int AmountOfPunchVariants => 3;
+        public override string PunchTexturePath => "JoJoStands/Projectiles/PlayerStands/PurpleHaze/PurpleHaze_Punch_";
+        public override Vector2 PunchSize => new Vector2(44, 12);
+        public override bool CanUsePart4Dye => false;
+        public override StandAttackType StandType => StandAttackType.Melee;
+
+        public override void AI()
+        {
+            SelectAnimation();
+            UpdateStandInfo();
+            UpdateStandSync();
+            if (shootCount > 0)
+                shootCount--;
+
+            Player player = Main.player[Projectile.owner];
+            MyPlayer mPlayer = player.GetModPlayer<MyPlayer>();
+            if (mPlayer.standOut)
+                Projectile.timeLeft = 2;
+
+            if (mPlayer.standControlStyle == MyPlayer.StandControlStyle.Manual)
+            {
+                if (Projectile.owner == Main.myPlayer)
+                {
+                    if (Main.mouseLeft)
+                        Punch();
+                    else
+                    {
+                        attacking = false;
+                        currentAnimationState = AnimationState.Idle;
+                    }
+                }
+                if (!attacking)
+                    StayBehind();
+            }
+            else if (mPlayer.standControlStyle == MyPlayer.StandControlStyle.Auto)
+                BasicPunchAI();
+
+            if (mPlayer.posing)
+                currentAnimationState = AnimationState.Pose;
+        }
+
+        public override void OnDyeChanged()
+        {
+            punchTextures = new Texture2D[AmountOfPunchVariants];
+            for (int v = 0; v < AmountOfPunchVariants; v++)
+                punchTextures[v] = ModContent.Request<Texture2D>(PunchTexturePath + (v + 1), AssetRequestMode.ImmediateLoad).Value;
+        }
+
+        public override void SelectAnimation()
+        {
+            if (oldAnimationState != currentAnimationState)
+            {
+                Projectile.frame = 0;
+                Projectile.frameCounter = 0;
+                oldAnimationState = currentAnimationState;
+                Projectile.netUpdate = true;
+            }
+
+            if (currentAnimationState == AnimationState.Idle)
+                PlayAnimation("Idle");
+            else if (currentAnimationState == AnimationState.Attack)
+                PlayAnimation("Attack");
+            else if (currentAnimationState == AnimationState.Pose)
+                PlayAnimation("Pose");
+        }
+
+        public override void PlayAnimation(string animationName)
+        {
+            if (Main.netMode != NetmodeID.Server)
+                standTexture = GetStandTexture("JoJoStands/Projectiles/PlayerStands/PurpleHaze", "PurpleHaze_" + animationName);
+
+            if (animationName == "Idle")
+                AnimateStand(animationName, 4, 12, true);
+            else if (animationName == "Attack")
+                AnimateStand(animationName, 4, newPunchTime, true);
+            else if (animationName == "Pose")
+                AnimateStand(animationName, 2, 12, true);
+        }
+    }
+}
---
+++ Projectiles/PlayerStands/PurpleHaze/PurpleHazeTiers.cs
@@ -0,0 +1,81 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Projectiles.PlayerStands.PurpleHaze
+{
+    public class PurpleHazeStandT1 : PurpleHazeStand
+    {
+        public override int TierNumber => 1;
+        public override int PunchDamage => 23;
+        public override int PunchTime => 11;
+        public override float MaxDistance => 98f;
+
+        protected override bool CanReleaseVirus => false;
+        protected override bool CanInfectOnHit => false;
+        protected override bool CanAOEBurst => false;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Projectile.width = 28;
+            Projectile.height = 56;
+        }
+    }
+
+    public class PurpleHazeStandT2 : PurpleHazeStand
+    {
+        public override int TierNumber => 2;
+        public override int PunchDamage => 34;
+        public override int PunchTime => 10;
+        public override float MaxDistance => 120f;
+
+        protected override bool CanReleaseVirus => true;
+        protected override bool CanInfectOnHit => false;
+        protected override bool CanAOEBurst => false;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Projectile.width = 30;
+            Projectile.height = 60;
+        }
+    }
+
+    public class PurpleHazeStandT3 : PurpleHazeStand
+    {
+        public override int TierNumber => 3;
+        public override int PunchDamage => 48;
+        public override int PunchTime => 9;
+        public override float MaxDistance => 140f;
+
+        protected override bool CanReleaseVirus => true;
+        protected override bool CanInfectOnHit => true;
+        protected override bool CanAOEBurst => false;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Projectile.width = 32;
+            Projectile.height = 64;
+        }
+    }
+
+    public class PurpleHazeStandFinal : PurpleHazeStand
+    {
+        public override int TierNumber => 4;
+        public override int PunchDamage => 65;
+        public override int PunchTime => 8;
+        public override float MaxDistance => 160f;
+
+        protected override bool CanReleaseVirus => true;
+        protected override bool CanInfectOnHit => true;
+        protected override bool CanAOEBurst => true;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            Projectile.width = 36;
+            Projectile.height = 70;
+        }
+    }
+}
