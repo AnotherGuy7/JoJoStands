322d7b3849504656645e683ff2c5b59e5b270a66
Jayl01 <50064874+AnotherGuy7@users.noreply.github.com>
2026-03-28 19:23:42 -0400

    Purple Haze Pt. 3

---
+++ Items/PurpleHazeT1.cs
@@ -0,0 +1,48 @@
+using JoJoStands.Items.CraftingMaterials;
+using JoJoStands.Tiles;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Items
+{
+    public class PurpleHazeT1 : StandItemClass
+    {
+        public override int StandSpeed => 16;
+        public override int StandType => 1;
+        public override string StandIdentifierName => "PurpleHaze";
+        public override int StandTier => 1;
+        public override Color StandTierDisplayColor => PurpleHazeFinal.PurpleHazeTierColor;
+
+
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Crazy Diamond (Tier 1)");
+            /* Tooltip.SetDefault(
+                "Left-click to punch enemies at a really fast rate!" +
+                "\nSpecial: Switch to Restoration Mode" +
+                "\nLeft-click in Restoration Mode to perform a restorative barrage and right-click to restore your item to it's component state." +
+                "\nUsed in Stand Slot"); */
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 9;
+            Item.width = 32;
+            Item.height = 32;
+            Item.maxStack = 1;
+            Item.value = 0;
+            Item.noUseGraphic = true;
+            Item.rare = ItemRarityID.LightPurple;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<StandArrow>())
+                .AddIngredient(ModContent.ItemType<WillToProtect>())
+                .AddTile(ModContent.TileType<RemixTableTile>())
+                .Register();
+        }
+    }
+}
