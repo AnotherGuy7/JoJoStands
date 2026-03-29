d1bfcc1a344a992df206ca426ba2124197c9bd88
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2026-03-29 18:56:59 +0300

    Flask of Purple Haze (mage item)

---
+++ Projectiles/FlaskOfPurpleHazeProj.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Projectiles
+{
+    public class FlaskOfPurpleHazeProj : ModProjectile
+    {
+        public override string Texture
+        {
+            get { return Mod.Name + "/Items/FlaskOfPurpleHaze"; }
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 14;
+            Projectile.height = 14;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = false;
+            Projectile.tileCollide = true;
+            Projectile.timeLeft = 10 * 60;
+            Projectile.penetrate = 1;
+            Projectile.alpha = 0;
+        }
+
+        public override void OnSpawn(Terraria.DataStructures.IEntitySource source)
+        {
+            Player owner = Main.player[Projectile.owner];
+            Projectile.ai[1] = owner.GetTotalDamage(DamageClass.Magic).Multiplicative;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation += 0.15f;
+
+            Projectile.velocity.Y += 0.12f;
+
+            if (Main.rand.NextBool(3))
+            {
+                Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height,
+                    ModContent.DustType<Dusts.GratefulDeadCloud>());
+                d.noGravity = true;
+                d.scale = 0.6f;
+                d.velocity *= 0.3f;
+            }
+        }
+
+        private void SpawnCloud()
+        {
+            if (Main.netMode != Terraria.ID.NetmodeID.MultiplayerClient)
+            {
+                int cloudIndex = Projectile.NewProjectile(
+                    Projectile.GetSource_FromThis(),
+                    Projectile.Center,
+                    Vector2.Zero,
+                    ModContent.ProjectileType<HazeVirusCloudFlask>(),
+                    0,
+                    0f,
+                    Projectile.owner);
+
+                if (cloudIndex >= 0 && cloudIndex < Main.maxProjectiles)
+                    Main.projectile[cloudIndex].ai[0] = Projectile.ai[1];
+            }
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            SpawnCloud();
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            return true;
+        }
+    }
+}
---
+++ Projectiles/HazeVirusCloudFlask.cs
@@ -0,0 +1,80 @@
+﻿using JoJoStands.Buffs.Debuffs;
+using JoJoStands.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Projectiles
+{
+    public class HazeVirusCloudFlask : ModProjectile
+    {
+        public override string Texture
+        {
+            get { return Mod.Name + "/Projectiles/ControllableNail"; }
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 2;
+            Projectile.height = 2;
+            Projectile.friendly = false;
+            Projectile.hostile = true;
+            Projectile.ignoreWater = false;
+            Projectile.tileCollide = false;
+            Projectile.alpha = 255;
+            Projectile.timeLeft = 30 * 60;
+        }
+
+        private const int gasRange = 8 * 16;
+
+        private float DamageMultiplier => Projectile.ai[0] > 0f ? Projectile.ai[0] : 1f;
+
+        public override void AI()
+        {
+            if (Projectile.localAI[0] == 0f)
+            {
+                HazeCloudBurst.Spawn(Projectile.Center, count: 5);
+                Projectile.localAI[0] = 1f;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                float angle = Main.rand.NextFloat(MathHelper.TwoPi);
+                float dist = Main.rand.NextFloat(gasRange);
+                Vector2 dustPos = Projectile.Center
+                    + new Vector2((float)System.Math.Cos(angle),
+                                  (float)System.Math.Sin(angle)) * dist;
+                Dust.NewDust(dustPos, 1, 1, ModContent.DustType<GratefulDeadCloud>());
+            }
+
+            for (int n = 0; n < Main.maxNPCs; n++)
+            {
+                NPC npc = Main.npc[n];
+                if (!npc.active || Projectile.Distance(npc.Center) >= gasRange)
+                    continue;
+
+                npc.AddBuff(ModContent.BuffType<HazeVirus>(), 30 * 60 * 60);
+
+                float bonusMultiplier = DamageMultiplier - 1f;
+                if (bonusMultiplier > 0f)
+                {
+                    int tileX = (int)(npc.Center.X / 16);
+                    int tileY = (int)(npc.Center.Y / 16);
+                    Color lightColor = Lighting.GetColor(tileX, tileY);
+                    float light = (lightColor.R + lightColor.G + lightColor.B) / (255f * 3f);
+                    float effectMultiplier = 1f - (MathHelper.Clamp(light, 0f, 1f) * 0.75f);
+
+                    int baseHPS = 2;
+                    npc.lifeRegen -= (int)(baseHPS * 2 * 120 * effectMultiplier * bonusMultiplier);
+                }
+            }
+
+            for (int p = 0; p < Main.maxPlayers; p++)
+            {
+                Player otherPlayer = Main.player[p];
+                if (otherPlayer.active && Projectile.Distance(otherPlayer.Center) < gasRange)
+                    otherPlayer.AddBuff(ModContent.BuffType<HazeVirus>(), 30 * 60 * 60);
+            }
+        }
+    }
+}
