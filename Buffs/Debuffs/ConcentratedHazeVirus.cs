bad565e117bbe41e77fd8004bdae4fbe7b130fda
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2026-03-29 20:12:36 +0300

    concentrated infection

--- Buffs/Debuffs/ConcentratedHazeVirus.cs
+++ Buffs/Debuffs/ConcentratedHazeVirus.cs
@@ -1,6 +1,7 @@
 using System;
 using Terraria;
 using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
 
 namespace JoJoStands.Buffs.Debuffs
 {
@@ -8,8 +9,6 @@ namespace JoJoStands.Buffs.Debuffs
     {
         public override void SetStaticDefaults()
         {
-            // DisplayName.SetDefault("Spin");
-            // Description.SetDefault("You're being infinitely spun... There's no hope in surviving.");
             Main.debuff[Type] = true;
             Main.buffNoTimeDisplay[Type] = true;
             Main.persistentBuff[Type] = true;
@@ -23,19 +22,18 @@ namespace JoJoStands.Buffs.Debuffs
             float light = (lightColor.R + lightColor.G + lightColor.B) / (255f * 3f);
             float lightFactor = MathHelper.Clamp(light / 1f, 0f, 1f);
             float effectMultiplier = 1f - (lightFactor * 0.75f);
-
             int maxDuration = light > 0.1f ? 10 * 60 : 20 * 60;
-
             int buffIndex = player.FindBuffIndex(Type);
             if (buffIndex >= 0 && player.buffTime[buffIndex] > maxDuration)
                 player.buffTime[buffIndex] = maxDuration;
-
             bool hasFilter = player.GetModPlayer<MyPlayer>().hazeVirusFilter;
             float filterMultiplier = hasFilter ? 0.2f : 1f;
-
             player.lifeRegen = (int)(-60 * effectMultiplier * filterMultiplier);
             player.moveSpeed /= 1f + (1f * effectMultiplier);
         }
+
+        private const int spreadRange = 12 * 16;
+        private const int spreadInterval = 2 * 60;
 
         public override void UpdateBuffOnNPC(NPC npc)
         {
@@ -45,17 +43,31 @@ namespace JoJoStands.Buffs.Debuffs
             float light = (lightColor.R + lightColor.G + lightColor.B) / (255f * 3f);
             float lightFactor = MathHelper.Clamp(light / 1f, 0f, 1f);
             float effectMultiplier = 1f - (lightFactor * 0.75f);
-
             int maxDuration = light > 0.1f ? 10 * 60 : 20 * 60;
-
             int buffIndex = npc.FindBuffIndex(Type);
             if (buffIndex >= 0 && npc.buffTime[buffIndex] > maxDuration)
                 npc.buffTime[buffIndex] = maxDuration;
-
             int healthASecond = 6 + GetDebuffOwnerModPlayer(npc).standTier;
             npc.lifeRegen -= (int)(healthASecond * 2 * 120 * effectMultiplier);
             if (Math.Abs(npc.velocity.X) > 1f)
                 npc.velocity.X *= 1f - (0.5f * effectMultiplier);
+            if (Main.GameUpdateCount % spreadInterval == 0)
+            {
+                int hazeVirusType = ModContent.BuffType<HazeVirus>();
+                int concentratedType = ModContent.BuffType<ConcentratedHazeVirus>();
+                for (int n = 0; n < Main.maxNPCs; n++)
+                {
+                    NPC other = Main.npc[n];
+                    if (!other.active || other.whoAmI == npc.whoAmI)
+                        continue;
+                    if (npc.Distance(other.Center) < spreadRange
+                        && other.FindBuffIndex(hazeVirusType) < 0
+                        && other.FindBuffIndex(concentratedType) < 0)
+                    {
+                        other.AddBuff(hazeVirusType, 10 * 60);
+                    }
+                }
+            }
         }
     }
 }
--- Projectiles/HazeVirusCloud.cs
+++ Projectiles/HazeVirusCloud.cs
@@ -3,7 +3,6 @@ using JoJoStands.Dusts;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
-
 namespace JoJoStands.Projectiles
 {
     public class HazeVirusCloud : ModProjectile
@@ -12,7 +11,6 @@ namespace JoJoStands.Projectiles
         {
             get { return Mod.Name + "/Projectiles/ControllableNail"; }
         }
-
         public override void SetDefaults()
         {
             Projectile.width = 2;
@@ -26,6 +24,9 @@ namespace JoJoStands.Projectiles
         }
 
         private int gasRange = 15 * 16;
+        private const int concentratedThresholdSeconds = 5;
+        private const int initialBuffTicks = 30 * 60 * 60;
+        private const int concentratedThresholdTicks = concentratedThresholdSeconds * 60;
 
         public override void AI()
         {
@@ -34,7 +35,6 @@ namespace JoJoStands.Projectiles
                 HazeCloudBurst.Spawn(Projectile.Center, count: 8);
                 Projectile.localAI[0] = 1f;
             }
-
             for (int i = 0; i < 3; i++)
             {
                 float angle = Main.rand.NextFloat(MathHelper.TwoPi);
@@ -42,22 +42,55 @@ namespace JoJoStands.Projectiles
                 Vector2 dustPos = Projectile.Center
                     + new Vector2((float)System.Math.Cos(angle),
                                   (float)System.Math.Sin(angle)) * dist;
-
                 Dust.NewDust(dustPos, 1, 1, ModContent.DustType<GratefulDeadCloud>());
             }
-
+            int hazeVirusType = ModContent.BuffType<HazeVirus>();
+            int concentratedType = ModContent.BuffType<ConcentratedHazeVirus>();
             for (int n = 0; n < Main.maxNPCs; n++)
             {
                 NPC npc = Main.npc[n];
-                if (npc.active && Projectile.Distance(npc.Center) < gasRange)
-                    npc.AddBuff(ModContent.BuffType<HazeVirus>(), 30 * 60 * 60);
-            }
+                if (!npc.active || Projectile.Distance(npc.Center) >= gasRange)
+                    continue;
+                int buffIndex = npc.FindBuffIndex(hazeVirusType);
+                if (buffIndex >= 0)
+                {
+                    int ticksRemaining = npc.buffTime[buffIndex];
+                    int ticksElapsed = initialBuffTicks - ticksRemaining;
+                    if (ticksElapsed >= concentratedThresholdTicks)
+                    {
+                        npc.AddBuff(concentratedType, 30 * 60 * 60);
+                        npc.DelBuff(buffIndex);
+                    }
+                    else
+                    {
 
+                    }
+                }
+                else if (npc.FindBuffIndex(concentratedType) < 0)
+                {
+                    npc.AddBuff(hazeVirusType, initialBuffTicks);
+                }
+            }
             for (int p = 0; p < Main.maxPlayers; p++)
             {
                 Player otherPlayer = Main.player[p];
-                if (otherPlayer.active && Projectile.Distance(otherPlayer.Center) < gasRange)
-                    otherPlayer.AddBuff(ModContent.BuffType<HazeVirus>(), 30 * 60 * 60);
+                if (!otherPlayer.active || Projectile.Distance(otherPlayer.Center) >= gasRange)
+                    continue;
+                int buffIndex = otherPlayer.FindBuffIndex(hazeVirusType);
+                if (buffIndex >= 0)
+                {
+                    int ticksRemaining = otherPlayer.buffTime[buffIndex];
+                    int ticksElapsed = initialBuffTicks - ticksRemaining;
+                    if (ticksElapsed >= concentratedThresholdTicks)
+                    {
+                        otherPlayer.AddBuff(concentratedType, 30 * 60 * 60);
+                        otherPlayer.DelBuff(buffIndex);
+                    }
+                }
+                else if (otherPlayer.FindBuffIndex(concentratedType) < 0)
+                {
+                    otherPlayer.AddBuff(hazeVirusType, initialBuffTicks);
+                }
             }
         }
     }
