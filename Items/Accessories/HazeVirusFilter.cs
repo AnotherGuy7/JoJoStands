b2e21f05c82da3c50a97d15579c4f85478b8107a
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2026-03-29 15:36:40 +0300

    Haze Virus Filter

---
+++ Items/Accessories/HazeVirusFilter.cs
@@ -0,0 +1,45 @@
+﻿using JoJoStands.Items.CraftingMaterials;
+using JoJoStands.Tiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JoJoStands.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Front)]
+    public class HazeVirusFilter : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Haze Virus Filter");
+            // Tooltip.SetDefault("Grants permanent immunity to the Haze Virus and Concentrated Haze Virus.");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
+            Item.accessory = true;
+            Item.rare = ItemRarityID.Pink;
+            Item.value = Item.buyPrice(0, 5, 0, 0);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.buffImmune[ModContent.BuffType<Buffs.Debuffs.HazeVirus>()] = true;
+            player.buffImmune[ModContent.BuffType<Buffs.Debuffs.ConcentratedHazeVirus>()] = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<GreaterPlatinumAmuletOfEscape>())
+                .AddIngredient(ModContent.ItemType<GreaterPlatinumAmuletOfChange>())
+                .AddIngredient(ModContent.ItemType<WillToChange>(), 2)
+                .AddIngredient(ModContent.ItemType<WillToEscape>(), 2)
+                .AddIngredient(ItemID.Ectoplasm, 5)
+                .AddTile(ModContent.TileType<RemixTableTile>())
+                .Register();
+        }
+    }
+}
