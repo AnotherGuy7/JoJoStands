35adba70f728722378a57c2ea125aac22aa429f3
Ozi6 <64336613+Ozi6@users.noreply.github.com>
2026-03-29 20:32:34 +0300

    cloud expands over time

--- Projectiles/HazeVirusCloud.cs
+++ Projectiles/HazeVirusCloud.cs
@@ -1,16 +1,16 @@
 using JoJoStands.Buffs.Debuffs;
 using JoJoStands.Dusts;
 using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.ModLoader;
+
 namespace JoJoStands.Projectiles
 {
     public class HazeVirusCloud : ModProjectile
     {
-        public override string Texture
-        {
-            get { return Mod.Name + "/Projectiles/ControllableNail"; }
-        }
+        public override string Texture => Mod.Name + "/Projectiles/ControllableNail";
+
         public override void SetDefaults()
         {
             Projectile.width = 2;
@@ -23,73 +23,87 @@ namespace JoJoStands.Projectiles
             Projectile.timeLeft = 30 * 60;
         }
 
-        private int gasRange = 15 * 16;
-        private const int concentratedThresholdSeconds = 5;
-        private const int initialBuffTicks = 30 * 60 * 60;
-        private const int concentratedThresholdTicks = concentratedThresholdSeconds * 60;
+        private const float MaxGasRange = 15 * 16;
+        private const int ExpansionTimeTicks = 60;
+        private const int ConcentratedThresholdSeconds = 5;
+        private const int InitialBuffTicks = 30 * 60 * 60;
+        private const int ConcentratedThresholdTicks = ConcentratedThresholdSeconds * 60;
 
         public override void AI()
         {
+            int ticksElapsed = (30 * 60) - Projectile.timeLeft;
+
+            float currentGasRange = MathHelper.Lerp(0, MaxGasRange, Math.Min((float)ticksElapsed / ExpansionTimeTicks, 1f));
+
             if (Projectile.localAI[0] == 0f)
             {
                 HazeCloudBurst.Spawn(Projectile.Center, count: 8);
                 Projectile.localAI[0] = 1f;
             }
+
             for (int i = 0; i < 3; i++)
             {
                 float angle = Main.rand.NextFloat(MathHelper.TwoPi);
-                float dist = Main.rand.NextFloat(gasRange);
-                Vector2 dustPos = Projectile.Center
-                    + new Vector2((float)System.Math.Cos(angle),
-                                  (float)System.Math.Sin(angle)) * dist;
+                float dist = Main.rand.NextFloat(currentGasRange);
+                Vector2 dustPos = Projectile.Center + angle.ToRotationVector2() * dist;
                 Dust.NewDust(dustPos, 1, 1, ModContent.DustType<GratefulDeadCloud>());
             }
+
             int hazeVirusType = ModContent.BuffType<HazeVirus>();
             int concentratedType = ModContent.BuffType<ConcentratedHazeVirus>();
+
             for (int n = 0; n < Main.maxNPCs; n++)
             {
                 NPC npc = Main.npc[n];
-                if (!npc.active || Projectile.Distance(npc.Center) >= gasRange)
+                if (!npc.active || npc.friendly || Projectile.Distance(npc.Center) >= currentGasRange)
                     continue;
-                int buffIndex = npc.FindBuffIndex(hazeVirusType);
-                if (buffIndex >= 0)
-                {
-                    int ticksRemaining = npc.buffTime[buffIndex];
-                    int ticksElapsed = initialBuffTicks - ticksRemaining;
-                    if (ticksElapsed >= concentratedThresholdTicks)
-                    {
-                        npc.AddBuff(concentratedType, 30 * 60 * 60);
-                        npc.DelBuff(buffIndex);
-                    }
-                    else
-                    {
 
-                    }
-                }
-                else if (npc.FindBuffIndex(concentratedType) < 0)
-                {
-                    npc.AddBuff(hazeVirusType, initialBuffTicks);
-                }
+                ApplyVirus(npc, hazeVirusType, concentratedType);
             }
+
             for (int p = 0; p < Main.maxPlayers; p++)
             {
-                Player otherPlayer = Main.player[p];
-                if (!otherPlayer.active || Projectile.Distance(otherPlayer.Center) >= gasRange)
+                Player player = Main.player[p];
+                if (!player.active || player.dead || Projectile.Distance(player.Center) >= currentGasRange)
                     continue;
-                int buffIndex = otherPlayer.FindBuffIndex(hazeVirusType);
-                if (buffIndex >= 0)
+
+                ApplyVirus(player, hazeVirusType, concentratedType);
+            }
+        }
+
+        private void ApplyVirus(Entity entity, int virusType, int concentratedType)
+        {
+            int[] buffTypes = entity is NPC n ? n.buffType : ((Player)entity).buffType;
+            int[] buffTimes = entity is NPC npc ? npc.buffTime : ((Player)entity).buffTime;
+
+            int buffIndex = -1;
+            for (int i = 0; i < 10; i++)
+            {
+                if (buffTypes[i] == virusType) { buffIndex = i; break; }
+            }
+
+            if (buffIndex >= 0)
+            {
+                int ticksRemaining = buffTimes[buffIndex];
+                int elapsed = InitialBuffTicks - ticksRemaining;
+                if (elapsed >= ConcentratedThresholdTicks)
                 {
-                    int ticksRemaining = otherPlayer.buffTime[buffIndex];
-                    int ticksElapsed = initialBuffTicks - ticksRemaining;
-                    if (ticksElapsed >= concentratedThresholdTicks)
-                    {
-                        otherPlayer.AddBuff(concentratedType, 30 * 60 * 60);
-                        otherPlayer.DelBuff(buffIndex);
-                    }
+                    if (entity is NPC targetNpc) targetNpc.AddBuff(concentratedType, 30 * 60 * 60);
+                    else ((Player)entity).AddBuff(concentratedType, 30 * 60 * 60);
+
+                    if (entity is NPC targetN) targetN.DelBuff(buffIndex);
+                    else ((Player)entity).DelBuff(buffIndex);
                 }
-                else if (otherPlayer.FindBuffIndex(concentratedType) < 0)
+            }
+            else
+            {
+                bool hasConcentrated = false;
+                for (int i = 0; i < 10; i++) if (buffTypes[i] == concentratedType) hasConcentrated = true;
+
+                if (!hasConcentrated)
                 {
-                    otherPlayer.AddBuff(hazeVirusType, initialBuffTicks);
+                    if (entity is NPC targetNpc) targetNpc.AddBuff(virusType, InitialBuffTicks);
+                    else ((Player)entity).AddBuff(virusType, InitialBuffTicks);
                 }
             }
         }
